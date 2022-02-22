12d2fa421d86b299fd548290e11cbd7878457c85
JRicardoAndrade <jricardoandradecosta@gmail.com>
2022-02-22 17:31:52 -0300

    Lendo, escrevendo e apagando

--- tecnicas_programacao/exercicio/exercicioArquivos.js
+++ tecnicas_programacao/exercicio/exercicioArquivos.js
@@ -1,14 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile('file.txt', 'utf-8', function(err, data){
-    fs.writeFile('fileResultado.txt', data, function(err){
+const filesPath = [path.join(__dirname, 'file.txt'), path.join(__dirname, 'file2.txt'), path.join(__dirname, 'file3.txt')];
+let data = '';
+
+filesPath.forEach((file) => {
+    data = data.concat(`${fs.readFileSync(file, 'utf-8')}\n`);
+})
+console.log('Arquivos lidos.\n');
+
+fs.writeFileSync('fileResultado.txt', data, function(err){
         if(err){
             console.log('erro');
         }
         console.log('Arquivo criado')
-        //fs.unlink('./tecnicas_programacao/exercicio/file.txt');
-    })
+        
 });
 
-fs.r
+filesPath.forEach((file) => {
+    fs.unlinkSync(file);
+});
+console.log(`Arquivos originais apagados.\n`);
